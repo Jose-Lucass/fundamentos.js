594eebf34f7f2c714c54dd4417bdd475914d0327
joselucas05 <joselucas3005@gmail.com>
2024-03-26 22:07:17 -0300

    feat: Start JavaScript

---
+++ fundamentos.js
@@ -0,0 +1,55 @@
+//consoles
+console.log("Este é o metodo log")
+
+console.warn("Esse é o metodo warn")
+
+console.error("Esse é o metodo error")
+
+console.table("Coloque um array aqui")
+
+console.info("Esse é o metodo info")
+
+//variaveis
+var banana = 15
+let morango = 15
+//variavel constante
+const uva = 10
+
+console.log(banana)
+console.log(morango)
+console.log(uva)
+console.log(uva)
+
+//tipo de dados
+
+const name ='José Lucas dos Santos Soares'
+let qtd = 5
+const isApproved = false
+let lasName = null
+let age 
+const language = ["JavaScript", "PHP", "Phyton"]
+const user = {email: 'cjcj@teste.com', password: 'teste123'}
+
+console.log(name)
+console.log(qtd)
+console.log(isApproved)
+console.log(lasName)
+console.log(age)
+console.table(language)
+console.log(user)
+
+
+
+
+const numero = [1,2,3,45,5,54,4]
+
+console.table(numero)
+
+
+let laranja = [
+  {email: "Sasssaas", password: "ASassaas" },
+  {email: "Sasssaas", password: "ASassaas" },
+  {email: "Sasssaas", password: "ASassaas" }
+]
+
+console.table(laranja)
